0dfd7fd26847b8c7890bb9bb2589c739e34405eb
= <ble9@uco.edu>
2020-08-24 22:06:38 -0500

    nitial com

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,8 @@ class PhotoMemoApp extends StatelessWidget {
       initialRoute: SignInScreen.routeName,
       routes: {
         SignInScreen.routeName: (context) => SignInScreen(),
+        HomeScreen.routeName: (context) => HomeScreen(),
+        AddScreen.routeName: (context) => AddScreen(),
       },
     );
   }
